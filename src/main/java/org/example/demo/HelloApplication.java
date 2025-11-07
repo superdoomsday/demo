ec3c6c513d712fac5a54e0be1bed23e33b114fe6
superdoomsday <2083911176@qq.com>
2025-11-07 11:48:24 +0800

    update one

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -15,5 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        System.out.println("jjjjjjjjjjjjjjjjjjj");
+        System.out.println("牛鼻");
     }
 }
